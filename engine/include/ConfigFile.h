21a4b77f5f1ba06f9abf5af262b69e9e8c7f9f0b
Tristan Dubé <tristan.dube@gmail.com>
2018-05-31 19:33:54 -0400

    Implementation of the Config file.
    When opening the file, all data is read and placed in a list. The
    function GetConfigValue reads the value from the list. There is also a
    safe version where the caller can provide a value in case no value is
    found.

--- engine/include/ConfigFile.h
+++ engine/include/ConfigFile.h
@@ -2,9 +2,16 @@
 
 #include "core.h"
 
+#include <vector>
 #include <string.h>
 #include <string>
 #include "pugixml.hpp"
+
+struct ENGINE_CLASS ConfigValue
+{
+    char* Name;
+    char* Value;
+};
 
 class ENGINE_CLASS ConfigFile
 {
@@ -13,10 +20,12 @@ public:
     ~ConfigFile();
 
     std::string GetConfigValue(std::string name);
+    std::string GetConfigValueSafe(std::string name, std::string valueIfNull);
 
 private:
     const char* LoadedFile;
     pugi::xml_document* doc;
+    std::vector<ConfigValue*>* values;
 
     bool isValid;
 };
--- engine/src/ConfigFile.cpp
+++ engine/src/ConfigFile.cpp
@@ -2,30 +2,113 @@
 
 ConfigFile::ConfigFile(const char *filepath)
 {
+    char msg[1024];
+    sprintf(msg, "ConfigFile reading %s", filepath);
+    LogTrace(msg);
+
     doc = new pugi::xml_document();
     pugi::xml_parse_result result = doc->load_file(filepath);
 
     if(result)
     {
+        sprintf(msg, "Config File %s opened successfully.", filepath);
+
+        LoadedFile = filepath;
         isValid = true;
+        values = new std::vector<ConfigValue*>();
+
+        auto configNodes = doc->select_nodes("/config/data");
+        auto begin = configNodes.begin();
+        auto end = configNodes.end();
+
+        while(begin != end)
+        {
+            pugi::xpath_node val = *begin;
+            
+            const char* name = val.node().attribute("name").as_string();
+            const char* valueString = val.node().text().as_string();
+
+            ConfigValue* valueStruct = new ConfigValue();
+            valueStruct->Name = new char[strlen(name)+1];
+            valueStruct->Value = new char[strlen(valueString)+1];
+
+            strcpy(valueStruct->Name, name);
+            strcpy(valueStruct->Value, valueString);
+
+            values->push_back(valueStruct);
+
+            begin++;
+        }
     }
     else
     {
+        sprintf(msg, "Failed to open file %s.", filepath);
+
+        LoadedFile = NULL;
         isValid = false;
+        values = NULL;
+        doc = NULL;
     }
 }
 
 ConfigFile::~ConfigFile()
 {
+    char msg[1024];
+    sprintf(msg, "Deleting config file %s", this->LoadedFile);
+    LogTrace(msg);
+
     delete(doc);
+    
+    auto begin = values->begin();
+    auto end = values->end();
+    while(begin != end)
+    {
+        ConfigValue* value = *begin;
+        delete[] value->Name;
+        delete[] value->Value;
+
+        delete(value);
+
+        begin++;
+    }
+
+    delete(values);
 }
 
 std::string ConfigFile::GetConfigValue(std::string name)
 {
-    char xpath[1024];
-    sprintf(xpath, "/config/data[@name='%s']", name.c_str());
+    auto begin = values->begin();
+    auto end = values->end();
+    while(begin != end)
+    {
+        ConfigValue* value = *begin;
 
-    auto x = doc->select_node(xpath);
+        if(value->Name == name)
+        {
+            return value->Value;
+        }
 
-    return x.node().text().as_string();
+        begin++;
+    }
+
+    return "";
+}
+
+std::string ConfigFile::GetConfigValueSafe(std::string name, std::string valueIfNull)
+{
+    auto begin = values->begin();
+    auto end = values->end();
+    while(begin != end)
+    {
+        ConfigValue* value = *begin;
+
+        if(value->Name == name)
+        {
+            return value->Value;
+        }
+
+        begin++;
+    }
+
+    return valueIfNull;
 }

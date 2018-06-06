f9fcc6e695bca4e0f5f0440ef26ccb9ffd744176
Tristan Dubé <tristan.dube@gmail.com>
2018-06-06 19:39:07 -0400

    Convenience include file.
    microengine.h can be included by consumers of the engine to have the
    most common included files.

---
+++ engine/include/microengine.h
@@ -0,0 +1,13 @@
+/* 
+* Common Engine includes
+* Designed to be included by MicroEngine consumers.
+* The engine itself should include all files individually.
+*/
+
+#include <GameEngine.h>
+#include <GameModule.h>
+#include <ARenderer.h>
+#include <ASprite.h>
+#include <AText.h>
+#include <AFont.h>
+#include <ATexture.h>

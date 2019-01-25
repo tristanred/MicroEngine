a6fe84e07881ea77e34180d3bdcbb684dec6a899
Tristan Dubé <tristan.dube@gmail.com>
2019-01-25 10:51:57 -0500

    Managed to specialize the Event class for void
    Turns out it wasn’t as hard as I thought. Two instances of both classes have been created, this creates a bit of code duplication but since I need to adapt the inner std::function member of the class I think I need to create a whole copy of the class.
    
    Both classe are the same except they deal with passing 1 or no argument to the dispatch function and declared the std::function with a parameter type or not.

--- engine/include/Events/Event.h
+++ engine/include/Events/Event.h
@@ -18,32 +18,86 @@
  * When a caller uses the .Listen method to attach a callback, a handle of type
  * EventHandle is returned. This object can be .Disposed() to stop listening
  * to that particular event. It also releases resources taken by the listener.
- *
- * Due to the way the class was programmed, the Event class takes a single
- * template parameter to indicate the type of parameter that is passed with an
- * event. If multiple parameters are required they have to be grouped in a
- * class. Also, if no parameters are required, we must use the stub-type
- * EVENT_VOID. This still passes a parameter to the callback functions but
- * it is an indicator that the parameter is unused. This distinction is needed
- * because the Event type takes a template T and std::functions are declared
- * like std::function<void(T)> to use 1 parameter but to use it without
- * parameters we must omit the T entirely, setting it to void, for example like
- * std::function<void(void)> does not work. Must be std::function<void()>.
- * It must be possible to have a solution due to SFINAE but I have to work out
- * the specifics.
  */
 
-struct EVENT_VOID
-{
-    unsigned int stub;
-};
 
 #include "core.h"
 #include <libtech/arraylist.h>
 #include <functional>
 
+/*
+ * Since both classes depend on each other, I am forward-declaring them both
+ * as early as possible.
+ */
 template <class T>
 class EventHandle;
+template <>
+class EventHandle<void>;
+
+template <class T>
+class Event;
+template <>
+class Event<void>;
+
+
+template <class T>
+class ENGINE_CLASS EventHandle
+{
+    friend class Event<T>;
+public:
+    EventHandle(Event<T>* target, std::function<void(T)> func)
+    {
+        this->isDisposed = false;
+        this->targetEvent = target;
+        this->targetFunction = func;
+    };
+    
+    ~EventHandle()
+    {
+    };
+    
+    void Dispose()
+    {
+        assert(targetEvent != NULL);
+        
+        if(this->isDisposed == false)
+        {
+            targetEvent->Listeners->RemoveObject(this);
+            this->isDisposed = true;
+        }
+    };
+    
+private:
+    bool isDisposed;
+    Event<T>* targetEvent;
+    std::function<void(T)> targetFunction;
+};
+
+template <>
+class ENGINE_CLASS EventHandle<void>
+{
+    friend class Event<void>;
+public:
+    EventHandle(Event<void>* target, std::function<void()> func)
+    {
+        this->isDisposed = false;
+        this->targetEvent = target;
+        this->targetFunction = func;
+    };
+    
+    ~EventHandle()
+    {
+    };
+    
+    void Dispose();
+    
+private:
+    bool isDisposed;
+    Event<void>* targetEvent;
+    std::function<void()> targetFunction;
+};
+
+
 
 template <class T>
 class ENGINE_CLASS Event
@@ -83,35 +137,51 @@ private:
     ArrayList<EventHandle<T>*>* Listeners;
 };
 
-template <class T>
-class ENGINE_CLASS EventHandle
+template <>
+class ENGINE_CLASS Event<void>
 {
-    friend class Event<T>;
+    friend class EventHandle<void>;
 public:
-    EventHandle(Event<T>* target, std::function<void(T)> func)
+    Event()
     {
-        this->isDisposed = false;
-        this->targetEvent = target;
-        this->targetFunction = func;
+        this->Listeners = new ArrayList<EventHandle<void>*>();
     };
-
-    ~EventHandle()
+    
+    ~Event()
     {
+        this->Listeners->Clear();
+        delete(this->Listeners);
     };
-
-    void Dispose()
+    
+    EventHandle<void>* Listen(std::function<void()> listenFunction)
     {
-        assert(targetEvent != NULL);
-
-        if(this->isDisposed == false)
+        EventHandle<void>* handle = new EventHandle<void>(this, listenFunction);
+        
+        this->Listeners->Add(handle);
+        
+        return handle;
+    };
+    
+    void Dispatch()
+    {
+        for(int i = 0; i < this->Listeners->Count(); i++)
         {
-            targetEvent->Listeners->RemoveObject(this);
-            this->isDisposed = true;
+            EventHandle<void>* handel = this->Listeners->Get(i);
+            handel->targetFunction();
         }
     };
-
+    
 private:
-    bool isDisposed;
-    Event<T>* targetEvent;
-    std::function<void(T)> targetFunction;
+    ArrayList<EventHandle<void>*>* Listeners;
 };
+
+void EventHandle<void>::Dispose()
+{
+    assert(targetEvent != NULL);
+    
+    if(this->isDisposed == false)
+    {
+        targetEvent->Listeners->RemoveObject(this);
+        this->isDisposed = true;
+    }
+}

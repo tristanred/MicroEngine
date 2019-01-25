a11e87ead15f6a8109a9a281afb819bdcc1e14be
Tristan Dubé <tristan.dube@gmail.com>
2019-01-24 21:39:39 -0500

    Rework to store event handles rather than std::function in the list
    This is because ArrayList does not work with values that are not
    pointers.

--- engine/include/Events/Event.h
+++ engine/include/Events/Event.h
@@ -1,5 +1,12 @@
 #pragma once
 
+/**
+ * Disable the warning about "std::function<void(T)> targetFunction;" not being exported
+ * with a dll-interface. This is ok in this case because it is a private member as referenced here
+ * https://docs.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-1-c4251
+ */
+#pragma warning(disable:4251)
+
 #include "core.h"
 #include <libtech/arraylist.h>
 #include <functional>
@@ -10,10 +17,11 @@ class EventHandle;
 template <class T>
 class ENGINE_CLASS Event
 {
+    friend class EventHandle<T>;
 public:
     Event()
     {
-        this->Listeners = new ArrayList<std::function<void(T)>>();
+        this->Listeners = new ArrayList<EventHandle<T>*>();
     };
 
     ~Event()
@@ -25,13 +33,22 @@ public:
     {
         EventHandle<T>* handle = new EventHandle<T>(this, listenFunction);
         
-        this->Listeners->Add(listenFunction);
+        this->Listeners->Add(handle);
         
         return handle;
     };
 
+    void Dispatch(T argument)
+    {
+        for(int i = 0; i < this->Listeners->Count(); i++)
+        {
+            EventHandle<T>* handel = this->Listeners->Get(i);
+            handel->targetFunction(argument);
+        }
+    };
+
 private:
-    ArrayList<std::function<void(T)>>* Listeners;
+    ArrayList<EventHandle<T>*>* Listeners;
 };
 
 template <class T>
@@ -39,7 +56,7 @@ class ENGINE_CLASS EventHandle
 {
     friend class Event<T>;
 public:
-    EventHandle(Event<T>* target, std::function<T> func)
+    EventHandle(Event<T>* target, std::function<void(T)> func)
     {
         this->isDisposed = false;
         this->targetEvent = target;
@@ -56,7 +73,7 @@ public:
 
         if(this->isDisposed == false)
         {
-            targetEvent->Listeners->RemoveObject(targetFunction);
+            targetEvent->Listeners->RemoveObject(this);
             this->isDisposed = true;
         }
     };
@@ -64,5 +81,5 @@ public:
 private:
     bool isDisposed;
     Event<T>* targetEvent;
-    std::function<T> targetFunction;
+    std::function<void(T)> targetFunction;
 };

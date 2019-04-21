eba7eb9545af4a6fccfce13b34b54238df5d9389
Tristan Dubé <tristan.dube@gmail.com>
2019-04-21 17:13:54 -0400

    APlatform documentation

--- engine/include/APlatform.h
+++ engine/include/APlatform.h
@@ -5,21 +5,35 @@
 class ARenderer;
 
 /**
- * Abstract platform class. The platform is the middle layer in charge of 
- * responding to specific platform events.
+ * \brief Abstract platform class.
+ *
+ * The platform is the middle layer in charge of responding to specific
+ * platform events.
  */
 class ENGINE_CLASS APlatform
 {
 public:
     APlatform(ARenderer* renderer);
     virtual ~APlatform();
-    
+
     // Platform State
     bool RequestExit;
-    
+
+    /**
+     * \brief Handle events raised by the current platorm.
+     *
+     * Usually used for pumping events out of the WIN32 message queue for
+     * example or Linux signals.
+     */
     virtual void HandleEvents() = 0;
 
+    /**
+     * \brief Clear resources associated with the this class.
+     *
+     * Usually called when the engine shuts itself down.
+     */
     virtual void Shutdown() = 0;
+
 protected:
     ARenderer* Renderer;
 };

de8cd974dff0fd313cd7c996acbe371dc25d83e8
Tristan Dubé <tristan.dube@gmail.com>
2019-04-22 22:39:21 -0400

    Start of ASprite doc

--- engine/include/ASprite.h
+++ engine/include/ASprite.h
@@ -6,6 +6,12 @@ class ATexture;
 #include "ARenderable.h"
 #include <libtech/arraylist.h>
 
+/**
+ * \brief Container class for a sprite's animation.
+ *
+ * An ASprite can have multiple instances of SpriteAnimation, one for each
+ * animation of the sprite.
+ */
 class ENGINE_CLASS SpriteAnimation
 {
 public:
@@ -13,15 +19,51 @@ public:
     int currentFrameIndex;
     ArrayList<ATexture*>* Textures;
 
+    /**
+     * \brief Set the name of this animation.
+     *
+     * A sprite uses the name of its animations to play them.
+     *
+     * \param name - Name of the animation
+     */
     void SetName(const char* name);
 
+    /**
+     * \brief Create a SpriteAnimation from a list of textures.
+     *
+     * This method takes all the textures in the list to make the animation.
+     *
+     * \param list - List of textures
+     * \return SpriteAnimation
+     */
     static SpriteAnimation* FromTextures(ArrayList<ATexture*>* list);
+
+    /**
+     * \brief Create a SpriteAnimation from a list of textures and a pattern
+     * to choose which frame from that list.
+     *
+     * This method is given a wide list of textures and the pattern is used to
+     * pick frames from the list. The pattern's format is like this 0..4 to
+     * choose the first 5 frames (because the last number is inclusive).
+     *
+     *
+     * \param list - List of textures
+     * \param pattern - String pattern to choose frames with.
+     * \return SpriteAnimation
+     */
     static SpriteAnimation* FromTextures(ArrayList<ATexture*>* list, const char* pattern);
+
+    /**
+     * \brief Create a SpriteAnimation from a variadic list of textures.
+     *
+     * The variadic argument is each texture that is going to make up the
+     * animation.
+     */
     static SpriteAnimation* FromTextures(ATexture*, ...); // Variadic of ATextures*
 };
 
 /*
- * A Sprite is a renderable object that has some playback mechanism
+ * \brief A Sprite is a renderable object that has some playback mechanism.
  *
  *
  *

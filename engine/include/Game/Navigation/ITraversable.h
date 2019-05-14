ecfa71dd3379e9c4c90380fee938295b49545a4f
Tristan Dubé <tristan.dube@gmail.com>
2019-05-13 22:56:09 -0400

    Added interface for navigation maps.

---
+++ engine/include/Game/Navigation/ITraversable.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "core.h"
+
+class ENGINE_CLASS ITraversable
+{
+public:
+
+    virtual bool IsPassable() = 0;
+
+    virtual ITraversable* Left() = 0;
+    virtual ITraversable* Up() = 0;
+    virtual ITraversable* Right() = 0;
+    virtual ITraversable* Down() = 0;
+};
